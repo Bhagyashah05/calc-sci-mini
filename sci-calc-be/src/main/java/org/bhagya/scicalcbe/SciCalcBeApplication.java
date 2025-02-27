a9922b73714be27b194383c52d1fa33ae836c0f8
bhagyashah05 <BhagyaShah.Ashishkumar2iiitb.ac.in>
2025-02-27 11:32:57 +0530

    docker file created and tested

--- sci-calc-be/src/main/java/org/bhagya/scicalcbe/SciCalcBeApplication.java
+++ sci-calc-be/src/main/java/org/bhagya/scicalcbe/SciCalcBeApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@CrossOrigin(origins = "http://localhost:3000")
 
 public class SciCalcBeApplication {
 
